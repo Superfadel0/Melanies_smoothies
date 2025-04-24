def23135aef80a733e0ae5d5c971ed0ba8fdfec5
Mouhamadou Fadel NDIAYE <126486272+Superfadel0@users.noreply.github.com>
2025-04-24 22:22:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
   """ Choose the fruits you want in your Custom Smoothie
   """
-)
+) 
 
 
 name_on_order = st.text_input("Name on order:")
